15eeaf4444b9db33ea076bcb0b34752d8734026f
Sam Venter <sventer444@gmail.com>
2019-12-20 15:16:55 -0500

    Add files via upload

--- 2019/problem1/answer2.js
+++ 2019/problem1/answer2.js
@@ -3,7 +3,10 @@ var sum =0;
 answer.forEach(calcFunc);
 
 function calcFunc(item){
-sum+= ((Math.floor(item/3))-2)
+if(item <=0){ return 0 }
+console.log(calcFunc(((Math.floor(item/3))-2)))
+sum+= calcFunc(((Math.floor(item/3))-2))
+return item
 }
 
 document.getElementById("output").innerHTML = sum
