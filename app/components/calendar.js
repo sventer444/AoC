3311e9dcf0abc31901f59d5ae8f02adc44394aaf
Sam Venter <sventer444@gmail.com>
2021-12-16 16:23:13 -0500

    switching to desktop and attempting to use sierra css

--- app/components/calendar.js
+++ app/components/calendar.js
@@ -1,7 +1,6 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
-import { action } from '@ember/object';
 
 export default class CalendarComponent extends Component {
   @service calendarManager;
