f568137143ca1ef94ebfd4c8fbaa2ac577bfde41
Sam Venter <sventer444@gmail.com>
2022-12-11 16:34:09 -0500

    port 443

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const LogicManager = require('./logic.js')
 const app = express()
-const port = process.env.PORT | 3000
+const port = process.env.PORT | 443
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
