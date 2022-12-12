b74e10285a9a081ea3deb28f63cb01b0885ad768
Sam Venter <sventer444@gmail.com>
2022-12-12 18:16:50 -0500

    trying to test site deploy

--- answers/2019/problem1/answer1.js
+++
@@ -1,9 +0,0 @@
-var answer=input.split("\n");
-var sum =0;
-answer.forEach(calcFunc);
-
-function calcFunc(item){
-sum+= ((Math.floor(item/3))-2)
-}
-
-document.getElementById("output").innerHTML = sum
--- index.js
+++ index.js
@@ -1,45 +1,17 @@
-const express = require('express')
-const LogicManager = require('./logic.js')
-const app = express()
-const port = process.env.PORT | 443
+const http = require('http');
 
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-app.use(express.static('public'))
+const port=process.env.PORT || 3000
 
-const server = app.listen(port, () => {
-    console.log(`Listening at localhost`)
-  })
-  const io = require('socket.io')(server)
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
 
-  app.get('/', (req, res) => {
-    res.render('index.html')
-  })
+res.end('<h1>Hello World</h1>');
 
-  io.on('connection', (socket) => {
-    var logicMan = new LogicManager()
+});
+
+server.listen(port,() => {
+
+    console.log(`Server running at port `+port);
     
-    socket.on('yearSelected', (year) => {
-        console.log("User selected year "+year)
-        logicMan.applyYear(year)
-    })
-
-    socket.on('daySelected', (day) => {
-        console.log("User selected day "+day)
-        var dayInput = logicMan.applyDay(day)
-        var answer = logicMan.fullAnswer(dayInput.toString())
-        socket.emit("answerPart1", answer.partOne)
-        socket.emit("answerPart2", answer.partTwo)
-      })
-
-    socket.on('asciiRequest', () => {
-      console.log('ascii requested, merry christmas')
-      var asciiResponse = logicMan.getRandomAscii()
-      socket.emit("asciiRecieved", asciiResponse)
-    })
-  
-    socket.on("connect_error", (err) => {
-      console.log(`connect_error due to ${err.message}`);
     });
-  
-  })//end io connection
