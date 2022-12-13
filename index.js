2bfbb580f07d948984caf47358b96d385bcee2eb
Sam Venter <sventer444@gmail.com>
2022-12-12 19:10:38 -0500

    attempting to use http server

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port=process.env.PORT || 3000
 const express = require('express')
 const LogicManager = require('./logic.js')
 const app = express()
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.static('public'))
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
@@ -34,41 +37,41 @@ server.listen(port,() => {
 //     res.setHeader('Content-Type', 'text/html');
 
 //     res.end('<h1>Hello World</h1>');
-//   })
-//   const io = require('socket.io')(server)
+  //})
+  const io = require('socket.io')(server)
 
-//   app.get('/', (req, res) => {
-//     res.render('index.html')
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'text/html');
+  app.get('/', (req, res) => {
+    //res.render('index.html')
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
 
-//     res.end('<h1>Hello World</h1>');
-//   })
+    res.end('<h1>Home Page</h1>');
+  })
 
-//   io.on('connection', (socket) => {
-//     var logicMan = new LogicManager()
+  io.on('connection', (socket) => {
+    var logicMan = new LogicManager()
     
-//     socket.on('yearSelected', (year) => {
-//         console.log("User selected year "+year)
-//         logicMan.applyYear(year)
-//     })
+    socket.on('yearSelected', (year) => {
+        console.log("User selected year "+year)
+        logicMan.applyYear(year)
+    })
 
-//     socket.on('daySelected', (day) => {
-//         console.log("User selected day "+day)
-//         var dayInput = logicMan.applyDay(day)
-//         var answer = logicMan.fullAnswer(dayInput.toString())
-//         socket.emit("answerPart1", answer.partOne)
-//         socket.emit("answerPart2", answer.partTwo)
-//       })
+    socket.on('daySelected', (day) => {
+        console.log("User selected day "+day)
+        var dayInput = logicMan.applyDay(day)
+        var answer = logicMan.fullAnswer(dayInput.toString())
+        socket.emit("answerPart1", answer.partOne)
+        socket.emit("answerPart2", answer.partTwo)
+      })
 
-//     socket.on('asciiRequest', () => {
-//       console.log('ascii requested, merry christmas')
-//       var asciiResponse = logicMan.getRandomAscii()
-//       socket.emit("asciiRecieved", asciiResponse)
-//     })
+    socket.on('asciiRequest', () => {
+      console.log('ascii requested, merry christmas')
+      var asciiResponse = logicMan.getRandomAscii()
+      socket.emit("asciiRecieved", asciiResponse)
+    })
   
-//     socket.on("connect_error", (err) => {
-//       console.log(`connect_error due to ${err.message}`);
-//     });
+    socket.on("connect_error", (err) => {
+      console.log(`connect_error due to ${err.message}`);
+    });
   
-//   })//end io connection
+  })//end io connection
