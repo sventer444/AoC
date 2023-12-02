4b074086a98c971a331912e66a8ec6fc7d4f5e00
Sam Venter <sventer444@gmail.com>
2023-12-02 12:25:17 -0500

    adjust logic of number parsing

--- answers/2023/day1/part1/answer.js
+++ answers/2023/day1/part1/answer.js
@@ -4,13 +4,13 @@ module.exports = class{
         this.splitInput = input.split(/\n\s*\n/)
         this.fullInputArray = this.splitInput[0].split("\n")
         //Cap array at 10 for now
-        this.fullInputArray.length = 10
+        // this.fullInputArray.length = 50
         // console.log('Count: '+this.fullInputArray.length)
     }
     answer(){
         this.answerValue = []
         var digitMap = this.fullInputArray.map(this.mapToDigits, this)
-        var digitSum = digitMap.reduce(this.findSum, 0, this)
+        var digitSum = this.findSum(digitMap)
         this.answerValue.push(`The total digit sum is ${digitSum}`)
         return this.answerValue
     }
@@ -21,28 +21,32 @@ module.exports = class{
             const firstNumIndex = value.search(/[0-9]/)
             const firstNum = value[firstNumIndex] 
             // console.log('First Number: ' +firstNum)
-            value = value.slice(firstNumIndex+1)
+            var splitValue = value.slice(firstNumIndex+1)
             //TODO Parse Second Integer in line
             //Reverse the string, then look for the 'first' number
-            var revVal = value.split('').reverse().join('')
+            var revVal = splitValue.split('').reverse().join('')
             // console.log(revVal)
             const lastNumIndex = revVal.search(/[0-9]/)
             const lastNum = revVal[lastNumIndex] 
             // console.log('Last Number' +lastNum)
 
             //TODO Return combination of first and second integers
-            var newNum = (lastNum != undefined)? `${firstNum}${lastNum}` : (firstNum != undefined)? `${firstNum}` : '0'
+            var newNum = (firstNum != undefined)? '0' : (lastNum != undefined)? `${firstNum}${firstNum}` : `${firstNum}${lastNum}`
             // console.log('New Number: ' +newNum)
-            // this.answerValue.push(`First Number: ${firstNum} + Last Number: ${lastNum} = ${newNum} <br>`)
+            this.answerValue.push(`Number '${newNum}' in '${value}' <br>`)
             return newNum
     }
-    findSum(total, input){
-        // console.log('Incoming Input: '+input)
-        var parsedNum = parseInt(input)
-        // console.log('Parsed int: '+ parsedNum)
-        total += parsedNum
-        console.log('Total: '+total)
-        return total
+    findSum(inputArray){
+        const sum = inputArray.reduce((total, input) => {
+            // console.log('Incoming Input: '+input)
+            var parsedNum = parseInt(input)
+            // console.log('Parsed int: '+ parsedNum)
+            // this.answerValue[index] = `Number: ${parsedNum} + Total: ${total} = ${(total + parsedNum)} <br>`
+            console.log(`Number: ${parsedNum} + Total: ${total} = ${(total + parsedNum)}`)
+            total += parsedNum
+            return total
+        }, 0)
+        return sum
     }
 }
 
