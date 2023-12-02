b05f5cf806e14fa31d574ddf9b08818ed3ae6d17
Sam Venter <sventer444@gmail.com>
2023-12-02 12:05:00 -0500

    add current work

--- answers/2023/day1/part1/answer.js
+++ answers/2023/day1/part1/answer.js
@@ -1,14 +1,48 @@
 
 module.exports = class{
     constructor(input){
-        this.fullInputArray = input.split(/\n\s*\n/)
+        this.splitInput = input.split(/\n\s*\n/)
+        this.fullInputArray = this.splitInput[0].split("\n")
+        //Cap array at 10 for now
+        this.fullInputArray.length = 10
+        // console.log('Count: '+this.fullInputArray.length)
     }
     answer(){
-        return this.cleanInput(this.fullInputArray)
+        this.answerValue = []
+        var digitMap = this.fullInputArray.map(this.mapToDigits, this)
+        var digitSum = digitMap.reduce(this.findSum, 0, this)
+        this.answerValue.push(`The total digit sum is ${digitSum}`)
+        return this.answerValue
     }
-    cleanInput(input){
-        console.log(input)
-        return input//.split("\n")
+    mapToDigits(value){
+        
+            //TODO Parse first integer in line
+            // console.log(value)
+            const firstNumIndex = value.search(/[0-9]/)
+            const firstNum = value[firstNumIndex] 
+            // console.log('First Number: ' +firstNum)
+            value = value.slice(firstNumIndex+1)
+            //TODO Parse Second Integer in line
+            //Reverse the string, then look for the 'first' number
+            var revVal = value.split('').reverse().join('')
+            // console.log(revVal)
+            const lastNumIndex = revVal.search(/[0-9]/)
+            const lastNum = revVal[lastNumIndex] 
+            // console.log('Last Number' +lastNum)
+
+            //TODO Return combination of first and second integers
+            var newNum = (lastNum != undefined)? `${firstNum}${lastNum}` : (firstNum != undefined)? `${firstNum}` : '0'
+            // console.log('New Number: ' +newNum)
+            // this.answerValue.push(`First Number: ${firstNum} + Last Number: ${lastNum} = ${newNum} <br>`)
+            return newNum
+    }
+    findSum(total, input){
+        // console.log('Incoming Input: '+input)
+        var parsedNum = parseInt(input)
+        // console.log('Parsed int: '+ parsedNum)
+        total += parsedNum
+        console.log('Total: '+total)
+        return total
     }
 }
 
